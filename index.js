f02a098ca017e308cd9b57110b6326daf7b7f9f5
kylekakota <kylekakota@gmail.com>
2025-05-01 15:49:04 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const reponse = document.getElementById("reponse");
-
-let liste = [];
-document.getElementById("btn-recherche").onclick = function rechercher() {
-  let valeur = document.getElementById("recherche").value;
-  liste.push(valeur);
-  liste.sort();
-  reponse.innerHTML = `${liste}<br>`;
-};
